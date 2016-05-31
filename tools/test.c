7e05be0efb6869faa042679cfc72075a25116c8d
augustin <augustin@augustin-16>
2016-05-31 09:42:41 +0800

    add *

--- tools/test.c
+++ tools/test.c
@@ -1,21 +1,26 @@
 #include <stdio.h>
 #include <time.h>
 #include <unistd.h>
+#include <stdlib.h>
 
-char c[1000000000]={0};
+#define MEM 2000000000
+
 
 int main(){
   time_t begin, end;
   long i;
+  char * c = calloc(MEM,sizeof(char));
   c[0]=0;
+
   while(1){
     /* Get the current time. */
     begin = time (NULL);
 
-    for( i = 0;i<100000;i++);
-
+    for( i = 0;i<MEM;i++){
+      c[i]=0;
+    }
     end = time(NULL);
 
-    sleep(difftime(end,begin));
+      //    sleep(difftime(end,begin));
   }
 }
