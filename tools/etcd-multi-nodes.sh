182e0c38f029873c0b909f0b93d160b666f403ca
root <root@augustine-01>
2016-07-19 00:54:43 -0400

    modify etcd-multi-nodes.sh

--- tools/etcd-multi-nodes.sh
+++ tools/etcd-multi-nodes.sh
@@ -1,27 +1,37 @@
 #!/bin/bash
 
-# more details for https://coreos.com/etcd/docs/latest
-echo "test: $h"
-which etcd &>/dev/null || { echo "etcd not installed, please install etcd first" && exit 1; }
+function usage
+{
+cat <<EOF 
+usage: ./etcd-multi-nodes.sh [[--discovery discovery_url] [--network_device eth1] [-h]]
+discovery_url can be get by:
+    curl -w "\n" 'https://discovery.etcd.io/new?size=2'
+note: change the size of etcd cluster
+EOF
+}
 
-if [ $# -eq 0 ] ; then
-    echo "Usage: `basename $0` ip1 ip2 ip3"
-    echo "    ip1 ip2 ip3 are the ip address of node etcd_1 etcd_2 etcd_3"
-    exit 1
-fi
-
-index=1
-while [ $# -gt 0 ] ; do
-    h="etcd_$index" 
-    if [ $index -eq 1 ] ; then
-        CLUSTER="$h=http://$1:2380"
-    else
-        CLUSTER="$CLUSTER,$h=http://$1:2380"
-    fi 
-    index=$(($index+1))
+[[ $# -eq 0 ]] && usage && exit 1
+while [ "$1" != "" ]; do
+    case $1 in
+	-d | --discovery )      shift
+				cluster_url=$1
+				;;
+	-n | --network_device ) shift
+	                        network_device=$1
+	                        ;;
+	-h | --help )           usage
+				exit
+				;;
+	* )                     usage
+				exit 1
+    esac
     shift
 done
 
+network_device=${network_device:-eth1}
+echo $network_device
+hostname=$(hostname)
+hostip=$(ifconfig $network_device | grep "inet addr" | cut -d ':' -f 2 | cut -d ' ' -f 1)
 # -initial-advertise-peer-urls  :  tell others what peer urls of me
 # -listen-peer-urls             :  what peer urls of me
 
@@ -32,11 +42,10 @@ done
 #                               :  new not means clear 
 
 
-etcd --name etcd_1 \
-     --initial-advertise-peer-urls http://$etcd_1:2380 \
-     --listen-peer-urls http://$etcd_1:2380 \
-     --listen-client-urls http://$etcd_1:2379 \
-     --advertise-client-urls http://$etcd_1:2379 \
-     --initial-cluster-token etcd-cluster \
-     --initial-cluster $CLUSTER \
+etcd --name $hostname \
+     --initial-advertise-peer-urls http://$hostip:2380 \
+     --listen-peer-urls http://$hostip:2380 \
+     --listen-client-urls http://$hostip:2379 \
+     --advertise-client-urls http://$hostip:2379 \
+     --discovery $cluster_url \
      --initial-cluster-state new 
