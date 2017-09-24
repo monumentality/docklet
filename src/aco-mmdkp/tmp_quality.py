7f25f68c7a9a70e6e8f50f4054fa9719a0402746
augustine.mars <augustine.programmer.mars@gmail.com>
2017-09-24 02:03:28 +0000

    ahah

---
+++ src/aco-mmdkp/tmp_quality.py
@@ -0,0 +1,14 @@
+import os
+import sys
+
+if __name__ == '__main__':
+#    os.system('rm -rf ../../test_result/quality_uniform_opt1.txt')
+    dis = sys.argv[1]
+    corr = sys.argv[2]
+
+    for i in [100]:
+        cmd = 'gcc -o relax_opt -DMACHINES='+str(i)+ ' -DDIS=\"\\\"'+dis+'\"\\\" -DCORR=\"\\\"'+corr+ '\"\\\" relax_opt.c'
+        print(cmd)
+        os.system(cmd)
+        os.system('./relax_opt')
+        
